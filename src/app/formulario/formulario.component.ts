6c537e0b94df7287d46125128cee1562497fd733
John Castañeda <bayron410@gmail.com>
2022-02-09 17:22:42 -0500

    Agregando mas servicios

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,38 +1,47 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LoggingService } from '../LoggingService.service';
 import { Persona } from '../persona.model';
+import { PersonasService } from '../personas.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
-
-  @Output() personaCreada = new EventEmitter<Persona>();
+  // @Output() personaCreada = new EventEmitter<Persona>();
 
   // nombreInput: string = "";
   // apellidoInput: string = "";
-  @ViewChild("nombreInput") nombreInput: ElementRef;
-  @ViewChild("apellidoInput") apellidoInput: ElementRef;
+  @ViewChild('nombreInput') nombreInput: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
-  constructor(private loggingService: LoggingService) { }
+  constructor(
+    private loggingService: LoggingService,
+    private personasService: PersonasService
+  ) {}
 
-  ngOnInit(): void {
-      
-  }
+  ngOnInit(): void {}
 
   agregarPersona() {
-    if(!(this.nombreInput.nativeElement.value == "" || this.apellidoInput.nativeElement.value == "")) {
+    if (
+      !(
+        this.nombreInput.nativeElement.value == '' ||
+        this.apellidoInput.nativeElement.value == ''
+      )
+    ) {
       // this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
-      let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
-      this.loggingService.enviarMensajeAConsola("Enviamos persona con nombre: " + persona1.nombre + " apellido: " + persona1.apellido);
-      this.personaCreada.emit(persona1);
-      this.nombreInput.nativeElement.value = "";
-      this.apellidoInput.nativeElement.value = "";
+      let persona1 = new Persona(
+        this.nombreInput.nativeElement.value,
+        this.apellidoInput.nativeElement.value
+      );
+      this.personasService.agregarPersona(persona1);
+      // this.loggingService.enviarMensajeAConsola("Enviamos persona con nombre: " + persona1.nombre + " apellido: " + persona1.apellido);
+      // this.personaCreada.emit(persona1);
+      this.nombreInput.nativeElement.value = '';
+      this.apellidoInput.nativeElement.value = '';
     } else {
-      alert("Llene los campos");
+      alert('Llene los campos');
     }
   }
-
 }
